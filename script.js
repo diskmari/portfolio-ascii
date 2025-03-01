cd271b6d593fc812e88302f703783a90ec3a0b25
diskmari <eumarimonteiro45140@gmail.com>
2025-02-28 21:13:30 -0300

    fix animation text

--- script.js
+++ script.js
@@ -61,3 +61,43 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+
+
+//
+
+const words = ['Front-end', 'Back-end', 'Full-stack'];
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+function typeEffect() {
+  const typedText = document.getElementById('typed-text');
+  const currentWord = words[wordIndex];
+
+  if (isDeleting) {
+    charIndex--;
+  } else {
+    charIndex++;
+  }
+
+  // Se o texto estiver vazio, adiciona um espaço em branco para evitar a quebra de linha
+  typedText.textContent = charIndex === 0 ? '\u00A0' : currentWord.substring(0, charIndex);
+
+  let typingSpeed = isDeleting ? 50 : 150;
+
+  if (!isDeleting && charIndex === currentWord.length) {
+    typingSpeed = 1000; // Tempo de espera antes de apagar
+    isDeleting = true;
+  } else if (isDeleting && charIndex === 0) {
+    isDeleting = false;
+    wordIndex = (wordIndex + 1) % words.length;
+    typingSpeed = 100; // Tempo de espera antes de digitar a próxima palavra
+  }
+
+  setTimeout(typeEffect, typingSpeed);
+}
+
+// Iniciar a animação quando a página carregar
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(typeEffect, 500);
+});
